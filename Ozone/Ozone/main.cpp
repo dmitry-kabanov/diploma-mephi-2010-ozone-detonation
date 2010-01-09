f735144d6d5dff8172e87cbac108500620bc462f
Dmitry Kabanov <kabanovdmitry@gmail.com>
2010-01-09 20:06:37 +0000

    Добавил в main.cpp код для вычисления времени выполнения программы.

--- Ozone/Ozone/main.cpp
+++ Ozone/Ozone/main.cpp
@@ -8,14 +8,23 @@
  * Главный файл проекта Ozone.
  */
 #include "main.h"
+#include <ctime>
+#include <iostream>
 #include "GodunovKolganMethod.h"
+using namespace std;
 
 int main(/*int argc, char *argv[]*/)
 {
 	Config config("Config.txt");
 
 	GodunovKolganMethod gkm(config);
+
+	clock_t start  = clock();
 	gkm.run();
+	clock_t finish = clock();
+
+	cout << "Calculations done in " <<
+		(double) (finish - start) / CLOCKS_PER_SEC << endl;
 
 	return 0;
 }
